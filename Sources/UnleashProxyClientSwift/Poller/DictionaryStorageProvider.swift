d0402e0b11f666fdc19925b071813821a55c97a8
kwasniew <kwasniewski.mateusz@gmail.com>
2025-07-21 14:47:34 +0200

    fix: dictionary write barrier

--- Sources/UnleashProxyClientSwift/Poller/DictionaryStorageProvider.swift
+++ Sources/UnleashProxyClientSwift/Poller/DictionaryStorageProvider.swift
@@ -2,12 +2,12 @@ import Foundation
 
 public class DictionaryStorageProvider: StorageProvider {
     private var storage: [String: Toggle] = [:]
-    private let queue = DispatchQueue(label: "com.unleash.storage")
+    private let queue = DispatchQueue(label: "com.unleash.storage", attributes: .concurrent)
 
     public init() {}
 
     public func set(values: [String: Toggle]) {
-        queue.async {
+        queue.async(flags: .barrier){
             self.storage = values
         }
     }
@@ -19,7 +19,7 @@ public class DictionaryStorageProvider: StorageProvider {
     }
 
     public func clear() {
-        queue.async {
+        queue.async(flags: .barrier) {
             self.storage = [:]
         }
     }
